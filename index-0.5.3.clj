d597ec3755522325f2f47cdb95b037059b9954d8
Tom Faulhaber <git_net@infolace.com>
2013-03-03 18:13:26 -0800

    Autodoc commit for master/07f3268e

--- index-0.5.3.clj
+++ index-0.5.3.clj
@@ -8,7 +8,21 @@
    :doc
    "core.memoize is a memoization library offering functionality above Clojure's core `memoize`\nfunction in the following ways:\n\n**Pluggable memoization**\n\ncore.memoize allows for different back-end cache implmentations to be used as appropriate without\nchanging the memoization modus operandi.\n\n**Manipulable memoization**\n\nBecause core.memoize allows you to access a function's memoization store, you do interesting things like\nclear it, modify it, and save it for later.\n"}),
  :vars
- ({:arglists ([cache-factory f & args]),
+ ({:arglists ([f cache]),
+   :name "->PluggableMemoization",
+   :namespace "clojure.core.memoize",
+   :source-url
+   "https://github.com/clojure/core.memoize/blob/e6e3aff9a7e0d782a373664506f0b859992b7d09/src/main/clojure/clojure/core/memoize.clj#L30",
+   :raw-source-url
+   "https://github.com/clojure/core.memoize/raw/e6e3aff9a7e0d782a373664506f0b859992b7d09/src/main/clojure/clojure/core/memoize.clj",
+   :wiki-url
+   "http://clojure.github.com/core.memoize//clojure.core.memoize-api.html#clojure.core.memoize/->PluggableMemoization",
+   :doc
+   "Positional factory function for class clojure.core.memoize.PluggableMemoization.",
+   :var-type "function",
+   :line 30,
+   :file "src/main/clojure/clojure/core/memoize.clj"}
+  {:arglists ([cache-factory f & args]),
    :name "build-memoizer",
    :namespace "clojure.core.memoize",
    :source-url
