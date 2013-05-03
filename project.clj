21d679df68044b11483ee9720911ff9e4d0ab9fd
Fogus <mefogus@gmail.com>
2013-05-03 13:31:18 -0300

    upgrading to latest clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject core.memoize "0.5.3-SNAPSHOT"
   :description "A memoization library for Clojure."
-  :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.cache "0.6.3"]]
   :plugins [[lein-swank "1.4.4"]
             [lein-marginalia "0.7.1"]]
