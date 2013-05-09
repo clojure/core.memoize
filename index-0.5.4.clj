b953684050139b8a45834aec0047e185427220a6
Tom Faulhaber <git_net@infolace.com>
2013-05-08 21:49:08 -0700

    Autodoc commit for master/21d679df

---
+++ index-0.5.4.clj
@@ -0,0 +1,18 @@
+{:namespaces
+ ({:source-url
+   "https://github.com/clojure/core.memoize/blob/61e3e9f7d871b0b137303d2fac48c40141cb7e71/src/main/clojure/clojure/core/memoize.clj",
+   :wiki-url
+   "http://clojure.github.com/core.memoize/clojure.core.memoize-api.html",
+   :name "clojure.core.memoize",
+   :author "fogus",
+   :doc
+   "core.memoize is a memoization library offering functionality above Clojure's core `memoize`\nfunction in the following ways:\n\n**Pluggable memoization**\n\ncore.memoize allows for different back-end cache implmentations to be used as appropriate without\nchanging the memoization modus operandi.\n\n**Manipulable memoization**\n\nBecause core.memoize allows you to access a function's memoization store, you do interesting things like\nclear it, modify it, and save it for later.\n"}),
+ :vars
+ ({:file nil,
+   :raw-source-url nil,
+   :source-url nil,
+   :wiki-url
+   "http://clojure.github.com/core.memoize//clojure.core.memoize-api.html#clojure.core.memoize/PluggableMemoization",
+   :namespace "clojure.core.memoize",
+   :var-type "type",
+   :name "PluggableMemoization"})}
