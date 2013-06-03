8308b391bce19bcc85b73fd9f77c27af4451d82a
Fogus <mefogus@gmail.com>
2013-06-03 10:03:37 -0400

    CMEMOIZE-5: Adding failing test for TTL race condition

--- src/test/clojure/clojure/core/memoize/tests.clj
+++ src/test/clojure/clojure/core/memoize/tests.clj
@@ -155,3 +155,12 @@
 (deftest test-snapshot-without-cache-field
   (testing "that we can call snapshot against an object without a 'cache' field"
     (is (= {} (snapshot (memo-pass-through identity 2000))))))
+
+(deftest test-regression-cmemoize-5
+  (testing "that the TTL doesn't bomb on race-condition"
+    (try
+      (let [id (memo-ttl identity 100)]
+        (dotimes [_ 10000000] (id 1)))
+      (is (= 1 1))
+      (catch NullPointerException npe
+        (is (= 1 0))))))
