e1ce7c2ad5ef8168a952d8747b204cc7d71dd440
Fogus <mefogus@gmail.com>
2013-06-04 13:39:46 -0400

    Update SNAP to cache SNAP

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject core.memoize "0.5.3-SNAPSHOT"
+(defproject core.memoize "0.5.5-SNAPSHOT"
   :description "A memoization library for Clojure."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.cache "0.6.3"]]
+                 [org.clojure/core.cache "0.6.4-SNAPSHOT"]]
   :plugins [[lein-swank "1.4.4"]
             [lein-marginalia "0.7.1"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
