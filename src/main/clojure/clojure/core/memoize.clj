9fc600e63760146a69f77313943398e17108e0d2
Fogus <mefogus@gmail.com>
2013-06-10 16:04:32 -0400

    Tweak deprecation method

--- src/main/clojure/clojure/core/memoize.clj
+++ src/main/clojure/clojure/core/memoize.clj
@@ -172,18 +172,7 @@
       ~@(for [[args body] arities]
           (list args `(!! (quote ~nom)) body))))
 
-(def-deprecated fifo
-  "DEPRECATED"
-  ([f] (memo-fifo f 32 {}))
-  ([f limit] (memo-fifo f limit {}))
-  ([f limit base]
-     (build-memoizer
-       #(PluggableMemoization. %1 (cache/fifo-cache-factory %3 :threshold %2))
-       f
-       limit
-       base)))
-
-(defn fifo
+(defn memo-fifo
   "Works the same as the basic memoization function (i.e. `memo` and `core.memoize` except
    when a given threshold is breached.  Observe the following:
 
