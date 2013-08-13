c32b2ae12ffdca9a1f76c3a4d2539fa0f6bcecbc
fogus <mefogus@gmail.com>
2013-08-13 08:13:15 -0400

    Bumped version in lein file

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject core.memoize "0.5.5-SNAPSHOT"
+(defproject core.memoize "0.5.7-SNAPSHOT"
   :description "A memoization library for Clojure."
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [;;[org.clojure/pom.contrib "0.1.2"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/core.cache "0.6.3"]]
   :plugins [[lein-swank "1.4.4"]
             [lein-marginalia "0.7.1"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :source-paths ["src/main/clojure"]
   :test-paths   ["src/test/clojure"])
+
