b904336d91888127001a7ddb72b998c4f1564381
fogus <mefogus@gmail.com>
2014-05-23 11:07:54 -0400

    Moving to core.cache SNAP version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject core.memoize "0.5.7-SNAPSHOT"
   :description "A memoization library for Clojure."
   :dependencies [;;[org.clojure/pom.contrib "0.1.2"]
-                 [org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.cache "0.6.3"]]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.cache "0.6.4-SNAPSHOT"]]
   :plugins [[lein-swank "1.4.4"]
             [lein-marginalia "0.7.1"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
