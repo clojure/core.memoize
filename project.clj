a0d0c493809d08986b2e7d847f6bd19394602aa6
puredanger <alex.miller@cognitect.com>
2014-11-12 08:11:06 -0600

    change core.cache dependency to stable version, add warn-on-reflection for lein build

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "A memoization library for Clojure."
   :dependencies [;;[org.clojure/pom.contrib "0.1.2"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.cache "0.6.4-SNAPSHOT"]]
+                 [org.clojure/core.cache "0.6.4"]]
   :plugins [[lein-swank "1.4.4"]
             [lein-marginalia "0.7.1"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :source-paths ["src/main/clojure"]
-  :test-paths   ["src/test/clojure"])
+  :test-paths   ["src/test/clojure"]
+  :global-vars {*warn-on-reflection* true})
 
